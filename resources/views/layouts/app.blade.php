48ab189a4b05a0d66d4819988d7195b9dd6025cc
Tristan Slater <hello@trslater.ca>
2023-09-27 16:59:22 -0700

    Setup Tailwind CSS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Posty</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="bg-gray-100">
